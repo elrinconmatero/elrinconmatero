4c0d4cdbe3f8fc3c840da7898859b21a4629f97b
elrinconmatero <robertotorres34@yahoo.com>
2025-11-10 13:47:39 -0400

    Add files via upload

---
+++ index (1).php
@@ -0,0 +1,25 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|ROBERT\\Administrador
+vti_modifiedby:SR|ROBERT1\\User
+vti_timelastmodified:TR|14 Feb 2014 21:33:39 -0000
+vti_timecreated:TR|13 Feb 2011 15:23:20 -0000
+vti_extenderversion:SR|6.0.2.5516
+vti_timelastindexed:TW|14 Aug 2016 16:57:40 -0000
+vti_backlinkinfo:VX|
+vti_nexttolasttimemodified:TR|13 Feb 2011 16:00:12 -0000
+vti_shadowfiles:VX|
+vti_syncwith_localhost\\c\:\\users\\user\\documents\\mis sitios web\\misitio/c\:/users/user/documents/mis sitios web/misitio:TR|14 Feb 2014 21:33:39 -0000
+vti_cacheddtm:TX|14 Aug 2016 16:57:40 -0000
+vti_filesize:IR|2603
+vti_cachedbodystyle:SR|<body>
+vti_cachedlinkinfo:VX|-S|_borders/rinconuevo.gif -K|left.htm -S|http://counter7.01counter.com/private/contadorvisitasgratis.php -H|Robert_Studio.htm -S|images/Robert1.jpg -S|_borders/Escudorincon.gif H|index.htm S|intro/images/intro.jpg -H|mailto:robertotorres34@yahoo.com
+vti_cachedsvcrellinks:VX|FHUS|index.htm FSUS|intro/images/intro.jpg
+vti_cachedneedsrewrite:BR|true
+vti_cachedhasbots:BR|true
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|true
+vti_botnavbits:SW|CHUT
+vti_botnavbarthemes:VR|sandston
+vti_botnavbarfeatures:VR|t
+vti_borderaggregate:SR|default
+vti_charset:SR|windows-1252
---
+++ index.php
@@ -0,0 +1,73 @@
+<html> 
+<head> 
+
+<script type="text/javascript"> 
+function redireccionar(){
+window.locationf="index.htm"; 
+}  
+setTimeout ("redireccionar()", 4000); //tiempo expresado en milisegundos 
+</script> 
+<meta name="Microsoft Border" content="tlrb, default">
+</head> 
+
+ <body><!--msnavigation--><table border="0" cellpadding="0" cellspacing="0" width="100%"><tr><td>
+
+<p align="center">
+<img border="0" src="_borders/rinconuevo.gif" width="1187" height="183" align="left"></p>
+<p align="center"></p>
+<p align="center"></p>
+<p align="center"></p>
+<p align="center"></p>
+<p align="center"></p>
+<p align="center"></p>
+<p align="center"></p>
+<p align="center"></p>
+<p align="center"></p>
+<p align="center"></p>
+<p align="center"></p>
+<p align="center"></p>
+<p align="center"></p>
+<p align="center"></p>
+<p align="center"></p>
+
+</td></tr><!--msnavigation--></table><!--msnavigation--><table dir="ltr" border="0" cellpadding="0" cellspacing="0" width="100%"><tr><td valign="top" width="1%">
+<p align="center">
+</p>
+<p align="center"><font size="2">Eres el Visitante<b> número:</b></font></p>
+<b>
+<table border="0" width="100%" style="border-collapse: collapse">
+	<tr>
+		<!--contador-->
+		<caption><a title="contador de visitas" href="#">
+		<img src="http://counter7.01counter.com/private/contadorvisitasgratis.php?c=3667684e4dd9f800113ba5151661f0de" border="0" title="contador de visita para paguina web" alt="contador de visita para paguina web"></a>
+		<!--contador--></caption>
+	</tr>
+</table>
+</b>
+<p align="center">&nbsp;</p>
+<p align="center"><a href="Robert_Studio.htm">
+<img border="0" src="images/Robert1.jpg" width="138" height="108"></a></p>
+<p align="center">
+<img border="0" src="_borders/Escudorincon.gif" width="136" height="162"></p>
+
+</td><td valign="top" width="24"></td><!--msnavigation--><td valign="top">
+ <center>Click en la imagen para entrar.</center>
+<p align="center"><a href="index.htm">
+<img border="0" src="intro/images/intro.jpg" width="920" height="695"></a></p>
+<!--msnavigation--></td><td valign="top" width="24"></td><td valign="top" width="1%">
+<p>&nbsp;</p>
+
+</td></tr><!--msnavigation--></table><!--msnavigation--><table border="0" cellpadding="0" cellspacing="0" width="100%"><tr><td>
+
+<center><small>
+</small><p>
+<small><a href="mailto:robertotorres34@yahoo.com?subject=Desde el Rincon">Si 
+tiene problemas o preguntas relacionadas con El Rincón del Arte Matero Contacte 
+aquí.<br>
+Última actualización:
+14 de agosto de 2016.</a></small></p>
+</center>
+
+</td></tr><!--msnavigation--></table></body> 
+
+</html>
